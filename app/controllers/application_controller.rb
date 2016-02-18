b9af08cabb245aa2b6e8fe075c7b341991423bf1
francis <sow.francis@gmail.com>
2016-02-18 23:59:20 +0100

    mise en place de profile utilisateur

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,8 +24,9 @@ class ApplicationController < ActionController::Base
 
 
   protected
+
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
-      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :lastname, :firstname, :avatar, :email, :password, :password_confirmation) }
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :lastname, :firstname, :avatar,:email, :password, :current_password) }
     end
 end
