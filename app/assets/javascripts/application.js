1b9b9c5098bfeb844e99b86df8fe9a2a57f0d610
francis <sow.francis@gmail.com>
2016-02-21 17:06:31 +0100

    sign out

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require twitter/bootstrap
 //= require jquery
 //= require jquery_ujs
+//= require twitter/bootstrap
 //= require_tree 
 //= require components
 //= require react
