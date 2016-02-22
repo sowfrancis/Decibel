a5988b2e91ef66c2782b223b9ca7a9b1bdf9aaef
francis <sow.francis@gmail.com>
2016-02-22 05:16:52 +0100

    mmm

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,3 @@
-Paperclip::Attachment.default_options[:s3_host_name] = 'decibelgroove.s3-website-us-east-1.amazonaws.com'
-Paperclip::Attachment.default_options[:url] = 's3-website-us-east-1.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-website-us-east-1.amazonaws.com'
+Paperclip::Attachment.default_options[:url] = 'decibelgroove.s3-website-us-east-1.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
