d3febdb3ff97a163e17d49ba6596847507969302
francis <sow.francis@gmail.com>
2016-02-22 04:41:14 +0100

    mm

--- app/models/track.rb
+++ app/models/track.rb
@@ -18,9 +18,9 @@ class Track < ActiveRecord::Base
                                   :s3_region => ENV['AWS_REGION'],
                                   :access_key_id =>  ENV['AWS_ACCESS_KEY_ID'],
                                   :secret_access_key =>  ENV['AWS_SECRET_ACCESS_KEY']
-                                  s3_host_name: 's3-website-us-east-1.amazonaws.com',
-                                  url: ":s3_domain_url",
-                                  path: '/:class/:attachment/:id_partition/:style/:filename'
+                                  :s3_host_name => 's3-website-us-east-1.amazonaws.com',
+                                  :url => ":s3_domain_url",
+                                  :path => '/:class/:attachment/:id_partition/:style/:filename'
                                  }
  
   validates_attachment_content_type :avatar, :content_type => [ 'image/png','image/jpeg']
