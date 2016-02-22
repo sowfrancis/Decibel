97185ad46f1b30ce9a9d2c10d04cf3dde7ddefdb
francis <sow.francis@gmail.com>
2016-02-22 05:47:37 +0100

    mmm

--- app/models/track.rb
+++ app/models/track.rb
@@ -18,8 +18,7 @@ class Track < ActiveRecord::Base
                                   :s3_region => ENV['AWS_REGION'],
                                   :access_key_id =>  ENV['AWS_ACCESS_KEY_ID'],
                                   :secret_access_key =>  ENV['AWS_SECRET_ACCESS_KEY'],
-                                  :s3_host_name => 's3-website-us-east-1.amazonaws.com',
-                                  :url => "decibelgroove.s3-website-us-east-1.amazonaws.com",
+                                  :url => ":s3_domain_url",
                                   :path => '/:class/:attachment/:id_partition/:style/:filename'
                                  }
  
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,3 @@
 Paperclip::Attachment.default_options[:s3_host_name] = 's3-website-us-east-1.amazonaws.com'
-Paperclip::Attachment.default_options[:url] = 'decibelgroove.s3-website-us-east-1.amazonaws.com'
+Paperclip::Attachment.default_options[:url] = 'http://grooving.s3-website-us-east-1.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
