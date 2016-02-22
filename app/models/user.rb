ce374d8ca851926877ee60406df579f7a24d601e
francis <sow.francis@gmail.com>
2016-02-22 03:02:55 +0100

    mm

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,16 @@ class User < ActiveRecord::Base
 
 
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, 
+                    default_url: "/images/:style/missing.png"
+                    :storage => :s3,
+                    :bucket => 'decibelgroove',
+                    :s3_credentials => {
+                      :access_key_id => "xxx",
+                      :secret_access_key => "xxx"
+                    }
+
+
   validates_attachment_content_type :avatar, :content_type => /.*/
 
   acts_as_messageable
