27889c3b13942e1b62fe724c2af5065475189d6d
francis <sow.francis@gmail.com>
2016-02-22 04:29:01 +0100

    encore

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@ class User < ActiveRecord::Base
                     :storage => :s3,
                     :s3_credentials => {
                       :bucket => ENV['S3_BUCKET_NAME'],
+                      :s3_region => ENV['AWS_REGION'],
                       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
                       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
                     }
