b8b6c2058d475e7eb35c0a4cd679e1d6e45d7ed0
francis <sow.francis@gmail.com>
2016-02-22 04:47:09 +0100

    mm

--- app/models/track.rb
+++ app/models/track.rb
@@ -17,7 +17,7 @@ class Track < ActiveRecord::Base
                                   :bucket => ENV['S3_BUCKET_NAME'],
                                   :s3_region => ENV['AWS_REGION'],
                                   :access_key_id =>  ENV['AWS_ACCESS_KEY_ID'],
-                                  :secret_access_key =>  ENV['AWS_SECRET_ACCESS_KEY']
+                                  :secret_access_key =>  ENV['AWS_SECRET_ACCESS_KEY'],
                                   :s3_host_name => 's3-website-us-east-1.amazonaws.com',
                                   :url => ":s3_domain_url",
                                   :path => '/:class/:attachment/:id_partition/:style/:filename'
